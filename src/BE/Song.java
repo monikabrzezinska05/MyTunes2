4de729a8d9d2caf92869dbf08243af692f324b83
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2022-11-28 11:22:23 +0100

    MyTunes

--- src/BE/Song.java
+++ src/BE/Song.java
@@ -1,6 +1,27 @@
 package BE;
 
 public class Song {
+
+    private int id;
+    private String title;
+    private int lenght;
+
+
     public Song(int id, String title, int length) {
+        this.id = id;
+        this.title = title;
+        this.lenght = length;
     }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public int getLenght() { return lenght; }
+
+    public void setLenght(int lenght) { this.lenght = lenght; }
 }
