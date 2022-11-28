72efedbaf61574baf5dadb55f1906fd10b4744d6
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2022-11-28 10:25:52 +0100

    MyTunes

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,7 +1,19 @@
 package GUI;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("View/SongView.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
