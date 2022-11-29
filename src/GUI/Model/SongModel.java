bc64f09b0cb0e90e94b85cdac0bdcc8f7d69ecd4
Janjarn <morgre01@easv365.dk>
2022-11-29 10:26:04 +0100

    songmodel

--- src/GUI/Model/SongModel.java
+++ src/GUI/Model/SongModel.java
@@ -1,17 +1,39 @@
 package GUI.Model;
 
+// Java imports
+import BE.Playlist;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 // Project imports
 import BLL.SongManager;
+import BE.Song;
+
+import java.util.List;
 
 public class SongModel {
 
+    private ObservableList<Song> songs;
+    private ObservableList<Playlist> playlist;
     private SongManager songManager;
 
     public SongModel() throws Exception {
         songManager = new SongManager();
-
+        songs = FXCollections.observableArrayList();
+        //songs.addAll(songManager.);
+        playlist = FXCollections.observableArrayList();
+        //songs.addAll(songManager.);
     }
 
+    public ObservableList<Song> getSongs() {
+        return songs;
+    }
 
+    public ObservableList<Playlist> getPlaylist() {
+        return playlist;
+    }
+
+    //public void searchSong(String query) throws Exception {
+      //  List<Song> searchResults = songManager.
+    //}
 }
