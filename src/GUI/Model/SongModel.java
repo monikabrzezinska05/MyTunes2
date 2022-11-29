afff8cca490abc5652014a0787c7d413f9fe1513
Janjarn <morgre01@easv365.dk>
2022-11-29 10:03:28 +0100

    fxml

--- src/GUI/Model/SongModel.java
+++ src/GUI/Model/SongModel.java
@@ -1,4 +1,15 @@
 package GUI.Model;
 
+import BLL.SongManager;
+
 public class SongModel {
+
+    private SongManager songManager;
+
+    public SongModel() throws Exception {
+        songManager = new SongManager();
+
+    }
+
+
 }
