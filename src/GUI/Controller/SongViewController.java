074125d27a17b4faef6485550650149f586c8c34
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2022-11-29 10:03:37 +0100

    MyTunes

--- src/GUI/Controller/SongViewController.java
+++ src/GUI/Controller/SongViewController.java
@@ -1,4 +1,39 @@
 package GUI.Controller;
 
-public class SongViewController {
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class SongViewController implements Initializable {
+
+    public TextField txtSongSearch;
+    public ListView<Songs> lstSongs;
+    public ListView<SongsInPlaylist> lstSongsInPlaylist;
+    public ListView<Playlists> lstPlaylists;
+
+    public Button newPlaylist;
+    public Button editPLaylist;
+    public Button deletePlaylist;
+
+    public Button deleteSongInPlaylist;
+    public Button newSong;
+    public Button editSong;
+    public Button playBtn;
+    public Button deleteSong;
+    public Button reverseBtn;
+    public Button forwardBtn;
+    public Button addSong;
+
+    public TextField searchBar;
+    public TextField
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
