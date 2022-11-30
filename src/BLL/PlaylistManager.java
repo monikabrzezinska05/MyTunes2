ad9ab58211a66d4b4340d737e23736409002c5a7
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2022-11-30 11:26:34 +0100

    MyTunes

--- src/BLL/PlaylistManager.java
+++ src/BLL/PlaylistManager.java
@@ -1,4 +1,20 @@
 package BLL;
 
+import BE.Playlist;
+import DAL.IPlaylistDataAccess;
+import DAL.db.PlaylistDAO_DB;
+
+import java.util.List;
+
 public class PlaylistManager {
+    private IPlaylistDataAccess playlistDAO;
+
+    public PlaylistManager() {
+        playlistDAO = new PlaylistDAO_DB();
+    }
+
+    public Playlist createPlaylist(String name) throws Exception {
+        return playlistDAO.createPlaylist(name);
+    }
+
 }
