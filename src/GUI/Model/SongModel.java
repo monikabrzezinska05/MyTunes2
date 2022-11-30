9cc184676d73aec8192ec86d293f5258e527eb8a
Janjarn <morgre01@easv365.dk>
2022-11-30 09:47:30 +0100

    songmodel

--- src/GUI/Model/SongModel.java
+++ src/GUI/Model/SongModel.java
@@ -33,7 +33,21 @@ public class SongModel {
         return playlist;
     }
 
-    //public void searchSong(String query) throws Exception {
-      //  List<Song> searchResults = songManager.
-    //}
+    public void searchSong(String query) throws Exception {
+        List<Song> searchResults = songManager.search(query);
+        songs.clear();
+        songs.addAll(searchResults);
+    }
+
+    /*public void searchPlaylist(String query) throws Exception {
+        List<Playlist> searchResults = songManager.search(query);
+        playlist.clear();
+        playlist.addAll(searchResults);
+    }*/
+
+    public void updateSong(Song updatedSong) throws Exception {
+        songManager.updateSong(updatedSong);
+        songs.clear();
+        songs.addAll(songManager.getSongs());
+    }
 }
