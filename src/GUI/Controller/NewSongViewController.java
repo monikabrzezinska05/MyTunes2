082ffc08055e2db119c6b423133989709f75c27f
Janjarn <morgre01@easv365.dk>
2022-12-01 11:11:08 +0100

    Update NewSongViewController.java

--- src/GUI/Controller/NewSongViewController.java
+++ src/GUI/Controller/NewSongViewController.java
@@ -38,6 +38,9 @@ public class NewSongViewController extends BaseController{
 
     }
 
+    public void handleSaveSong(ActionEvent actionEvent) {
+    }
+
     /*public void handleSaveSong(ActionEvent actionEvent) {
         try {
         } catch
