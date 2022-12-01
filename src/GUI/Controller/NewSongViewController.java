8e69e95616c08d22911e15b923452d854925600c
Janjarn <morgre01@easv365.dk>
2022-12-01 10:52:04 +0100

    newsongcontroller

--- src/GUI/Controller/NewSongViewController.java
+++ src/GUI/Controller/NewSongViewController.java
@@ -1,5 +1,42 @@
 package GUI.Controller;
 
-public class NewSongViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 
+public class NewSongViewController extends BaseController{
+
+    @FXML
+    private Button chooseFIle;
+    @FXML
+    private Button cancelSong;
+    @FXML
+    private Button saveSong;
+    @FXML
+    private TextField txtTitle;
+    @FXML
+    private TextField txtArtist;
+    @FXML
+    private TextField txtTime;
+    @FXML
+    private TextField txtFile;
+    @FXML
+    private MenuButton categoryDropdown;
+    
+
+    @Override
+    public void setup() {
+        
+    }
+
+    public void handleCategoryDropdown(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelSong(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveSong(ActionEvent actionEvent) {
+    }
 }
