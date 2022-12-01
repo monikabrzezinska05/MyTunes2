914879b0576561c1286ddc370e1641c8ef20b30e
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2022-12-01 11:11:51 +0100

    MyTunes

--- src/GUI/Controller/NewSongViewController.java
+++ src/GUI/Controller/NewSongViewController.java
@@ -4,7 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class NewSongViewController extends BaseController{
 
@@ -32,10 +34,16 @@ public class NewSongViewController extends BaseController{
     }
 
     public void handleCategoryDropdown(ActionEvent actionEvent) {
+        MenuButton menuButton = new MenuButton("Category");
+        menuButton.getItems().addAll(new MenuItem("Pop"), new MenuItem("HipHop"));
     }
 
     public void handleCancelSong(ActionEvent actionEvent) {
+        Stage stage = (Stage) cancelSong.getScene().getWindow();
+        stage.close();
+    }
 
+    public void handleSaveSong(ActionEvent actionEvent) {
     }
 
     /*public void handleSaveSong(ActionEvent actionEvent) {
