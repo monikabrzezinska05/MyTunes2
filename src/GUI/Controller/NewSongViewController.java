2516fac0b2ad85348e0f204aaf4e9daefb4850d5
Janjarn <morgre01@easv365.dk>
2022-12-02 13:55:28 +0100

    Revert "Merge branch 'master' of https://github.com/monikabrzezinska05/MyTunes2"
    This reverts commit 713237f3ec56e58cbd7269dcb2a5e2303bb7edb4, reversing
    changes made to 2ea8a7d2d47efffdd95fe0cf965419ad929b4872.

--- src/GUI/Controller/NewSongViewController.java
+++ src/GUI/Controller/NewSongViewController.java
@@ -60,9 +60,6 @@ public class NewSongViewController extends BaseController implements Initializab
         String category = categoryDropdown.getSelectionModel().getSelectedItem();
         int time = Integer.parseInt(txtTime.getText());
 
-        Stage stage = (Stage) saveSong.getScene().getWindow();
-        stage.close();
-
         try {
             songModel.createSong(title, artist, category, time);
         } catch (Exception e) {
